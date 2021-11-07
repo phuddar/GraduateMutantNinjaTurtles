887a6bae5d36810b508d9969cde0ede67901ab01
Jacob Joseph <jakejose@iu.edu>
2021-11-07 12:43:23 -0500

    Refs create, update, delete

--- app/public/js/directoryData.js
+++ app/public/js/directoryData.js
@@ -1,82 +1,117 @@
 const SomeApp = {
-    data() {
-      return {
-          refs: {},
-          curID: [],
-          games: [],
-          weather: {},
-          current: {},
-          feed: {}
-
+  data() {
+    return {
+      refs: {},
+      curID: [],
+      games: [],
+      current: {},
+      refForm: {},
+      check: "NO",
+    };
+  },
+  computed: {},
+  methods: {
+    getRefData() {
+      fetch("/api/refs/")
+        .then((response) => response.json())
+        .then((responseJson) => {
+          //console.log(responseJson);
+          this.refs = responseJson;
+          this.resetRefForm();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    selectRef(i) {
+      this.curID = i;
+      //console.log(this.curID);
+      this.generateGame(this.curID);
+    },
+    generateGame() {
+      fetch("/api/games/get.php")
+        //fetch('/api/games/')
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.games = responseJson;
+          //   console.log(this.games);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    selectedRef(r) {
+      this.refForm = r;
+      this.check = "YES";
+      //console.log(this.current);
+    },
+    postRef(evt) {
+      if (this.check == "NO") {
+        this.postNewRef(evt);
+      } else {
+        this.postEditRef(evt);
       }
     },
-    computed: {},
-    methods: {
-        getRefData(){
-            fetch('/api/refs/')
-          .then( response => response.json() )
-          .then( (responseJson) => {
-              //console.log(responseJson);
-              this.refs = responseJson;
-          })
-          .catch( (err) => {
-              console.error(err);
-          })
+    postNewRef(evt) {
+      this.refForm.Referee_ID = this.refs.length + 1;
+      console.log("Posting!", this.refForm);
+
+      fetch("api/refs/create.php", {
+        method: "POST",
+        body: JSON.stringify(this.refForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Accept: "application/json",
         },
-        selectRef(i){
-            this.curID = i;
-            //console.log(this.curID);
-            this.generateGame(this.curID);
-        },
-        generateGame(){
-           fetch('/api/games/get.php')
-        //fetch('/api/games/')
-          .then( response => response.json() )
-          .then( (responseJson) => {
-              
-              this.games = responseJson;
-            //   console.log(this.games);
-          })
-          .catch( (err) => {
-              console.error(err);
-          });
-        },
-        getWeather(){
-            fetch(`https://api.weatherapi.com/v1/current.json?key=1a7ba1d83faf49828d405436210511&q=London&aqi=no`)
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                // console.log(responseJson);
-                this.weather = responseJson;
-                // console.log(this.weather.current.condition.icon);
-                
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-          },
-          getTwitter(){
-            fetch(`https://www.thesportsdb.com/api/v1/json/1/searchfilename.php?e=English_Premier_League`)
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                //console.log(responseJson);
-                this.feed = responseJson.event;
-                console.log(this.feed)
-                console.log('here!');
-                console.log(this.feed.strThumb);
-                
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-          }
+      })
+        .then((next) => {
+          this.getRefData();
+        })
+        .catch((err) => console.error(err));
     },
-    created() {
-        this.getRefData();
-        this.generateGame();
-        this.getWeather();
-        this.getTwitter();
+    postEditRef(evt) {
+      console.log("hello");
+      console.log(this.refForm);
+      console.log("Posting!", this.refForm);
+
+      fetch("api/refs/update.php", {
+        method: "POST",
+        body: JSON.stringify(this.refForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Accept: "application/json",
+        },
+      })
+        .then((next) => {
+          this.getRefData();
+        })
+        .catch((err) => console.error(err));
+    },
+    deleteRef(r) {
+        console.log("Deleting!", r);
+
+        fetch("api/refs/delete.php", {
+          method: "POST",
+          body: JSON.stringify(r),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            Accept: "application/json",
+          },
+        })
+          .then((next) => {
+            this.getRefData();
+          })
+          .catch((err) => console.error(err));
+    },
+    resetRefForm() {
+      this.refForm = {};
+      this.check = "NO";
     }
-  
-  }
-  
-  Vue.createApp(SomeApp).mount('#refereeGame');
+  },
+  created() {
+    this.getRefData();
+    this.generateGame();
+  },
+};
+
+Vue.createApp(SomeApp).mount("#refereeGame");
